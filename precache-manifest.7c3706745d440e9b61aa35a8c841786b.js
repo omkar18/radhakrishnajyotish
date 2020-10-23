7ccc2c70cc41650fa754eb852bb1d8f030ad97c2
omkarwalujkar18 <omkarwalujkar18@gmail.com>
2020-10-23 22:33:37 +0530

    Updates

---
+++ precache-manifest.7c3706745d440e9b61aa35a8c841786b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08dfc8cde4182a6058f0da4d84d798af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0492ec705c7e0659efd3",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "972140d268ff317b1901",
+    "url": "/static/css/main.cdc01424.chunk.css"
+  },
+  {
+    "revision": "0492ec705c7e0659efd3",
+    "url": "/static/js/2.15259c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15259c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972140d268ff317b1901",
+    "url": "/static/js/main.1208c268.chunk.js"
+  },
+  {
+    "revision": "441ffbfb51627a67c334",
+    "url": "/static/js/runtime-main.2e4b9418.js"
+  },
+  {
+    "revision": "d500e03e29261f62c4eed4cafa9809e5",
+    "url": "/static/media/award1.d500e03e.jpg"
+  },
+  {
+    "revision": "9755c18a673aa8d75d6e2cb0ef511d41",
+    "url": "/static/media/award2.9755c18a.jpg"
+  },
+  {
+    "revision": "6f33bc79078e93f4df91d79b03a1110a",
+    "url": "/static/media/award4.6f33bc79.jpg"
+  },
+  {
+    "revision": "575cffb5f9a3289b2134864eab8c29c0",
+    "url": "/static/media/award5.575cffb5.jpg"
+  },
+  {
+    "revision": "c53b768979f54c83329b5cc138402067",
+    "url": "/static/media/award6.c53b7689.jpg"
+  },
+  {
+    "revision": "bf677f40c679b16b6d13d43f9d1262b7",
+    "url": "/static/media/award8.bf677f40.jpg"
+  },
+  {
+    "revision": "0d82123eb9da5d19a14891ddd9d25ae0",
+    "url": "/static/media/award9.0d82123e.jpeg"
+  },
+  {
+    "revision": "a39a8fe4ee53d7e0f395a7e450e51c63",
+    "url": "/static/media/bg1.a39a8fe4.png"
+  },
+  {
+    "revision": "a1231435fcd77f289859b8743b72a228",
+    "url": "/static/media/cta-bg.a1231435.jpg"
+  },
+  {
+    "revision": "89ddd33c66fca693d6ae7be9366144ab",
+    "url": "/static/media/dr. Arun Unchegaonkar.89ddd33c.jpeg"
+  },
+  {
+    "revision": "b95a1b5caa83ec684b8470400cdcd244",
+    "url": "/static/media/my_profile.b95a1b5c.png"
+  },
+  {
+    "revision": "183c33ce36d786a461e8b6ae0110d03f",
+    "url": "/static/media/newLogo.183c33ce.png"
+  },
+  {
+    "revision": "7d542ba46179f1e2ed62cb9637664e18",
+    "url": "/static/media/radhakrushna joytishya.7d542ba4.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/radhakrishnajyotish/precache-manifest.12b001196d14cc1aa828b71792c75e22.js"
+  "/precache-manifest.7c3706745d440e9b61aa35a8c841786b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/radhakrishnajyotish/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
