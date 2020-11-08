12b1ce2e14d4416cfb4be0b151ffc407827fd02d
omkarwalujkar18 <omkarwalujkar18@gmail.com>
2020-11-08 16:28:18 +0530

    Added Book Appointment Form

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,6 +11,30 @@ export class Hero extends Component {
             <h2>Perfect Analysis | Effective Solution</h2>
             <a href="#about" class="btn-get-started scrollto">
               Let's Connect            </a>
+            {/* <a href="#" data-target="#exampleModal" data-whatever="@mdo" class="btn-get-started scrollto">
+              Book Your Appoinment
+             </a> */}
+            <a href="#" class="btn-get-started scrollto" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Book Your Appoinment</a>
+
+          </div>
+
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Book Your Appointment At RadhaKrishna jyotish</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  {/* Google Form */}
+                  {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSciIcJ-Y7944eWhEkUB3xV8nXZZjz1elRI26fPzt9d0rPnY6g/viewform?embedded=true" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+
+                  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSciIcJ-Y7944eWhEkUB3xV8nXZZjz1elRI26fPzt9d0rPnY6g/viewform?embedded=true" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
         {/* <!-- End Hero --> */}
